07c23486645bd192212c3e32adccf88dfcb62ae9
DreadSteel99 <41543106+DreadSteel99@users.noreply.github.com>
2019-01-29 10:44:19 +0530

    Create inputs.java
    This program has an already declared int , double, String variable declared in its body, above which we accept three more values from the user.
    After that you concatenate the variables and display them.

---
+++ easy_level/inputs.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+       
+        /* Declared and read second integer, double, and String variables. */
+        
+        int a=Integer.parseInt(scan.nextLine());
+        
+        /*String input to Integer*/
+        
+        double b=Double.parseDouble(scan.nextLine());
+        
+        /*String input to Double*/
+        
+        String c=scan.nextLine();
+        
+        /* Print the sum of both integer variables on a new line. */
+        
+        System.out.println(i+a);
+        
+        /* Print the sum of the double variables on a new line. */
+        
+		    System.out.println(d+b);
+        
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+        
+        System.out.println(s+c);
+        
+        /*Closing the object of Scanner class*/
+        scan.close();
+    }
+}
